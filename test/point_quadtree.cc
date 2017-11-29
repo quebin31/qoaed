68c409a5510203273a71a860bc8f8f562b4d72b4
Kevin <lans9831@gmail.com>
2017-11-29 13:50:04 -0500

    added point quadtree test remove dummy target in cmake

---
+++ test/point_quadtree.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "point_quadtree.h"
+
+int main() {
+  qoaed::PointQuadtree<std::string> pqt(50, 50);
+
+  pqt.insert(3, 2, "P: 3");
+  pqt.insert(6, 7, "P: 2");
+  pqt.insert(4, 5, "P: 9");
+  pqt.insert(34, 2, "P: x");
+  pqt.insert(14, 9, "P: y");
+
+  auto result = pqt.ranged_query(qoaed::PointQuadtree<std::string>::Rect(1, 1, 10, 10));
+
+  for (auto& x : result)
+    std::cout << *x << ' ';
+  std::cout << std::endl;
+
+
+  return 0;
+}
