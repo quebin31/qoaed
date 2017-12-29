71419863a4e74e1bed502d0b2b13feffe09648b1
Kevin <lans9831@gmail.com>
2017-12-29 18:14:41 -0500

    reorganized project for now

---
+++ test/RegionOctreeTest.cpp
@@ -0,0 +1,5 @@
+// Test file
+// Make your test for Region Octree here
+// #include "RegionOctree.hpp"
+
+int main() { return 0; }
---
+++ test/RegionQuadtreeTest.cpp
@@ -0,0 +1,5 @@
+// Test file
+// Make your test for Region Quadtree here
+// #include "RegionQuadtree.hpp"
+
+int main() { return 0; }
