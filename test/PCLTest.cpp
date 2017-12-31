a7d0faff215591c50d6ac2100a25c33e5cd105c7
Kevin <lans9831@gmail.com>
2017-12-30 19:10:19 -0500

    working off together with pcl

--- test/PCLTest.cpp
+++ test/PCLTest.cpp
@@ -1,29 +1,19 @@
 #include <pcl/visualization/cloud_viewer.h>
 #include <iostream>
 
-int main() {
+#include "Tools.hpp"
+
+int main(int argc, char** argv) {
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PointXYZ p;
-  p.x = 3;
-  p.y = 3;
-  p.z = 1;
-  cloud->push_back(p);
-  p.x = 1;
-  p.y = 1;
-  p.z = 1;
-  cloud->push_back(p);
-  p.x = 4;
-  p.y = 7;
-  p.z = 2;
-  cloud->push_back(p);
-  p.x = 1;
-  p.y = 5;
-  p.z = 4;
-  cloud->push_back(p);
-  p.x = 5;
-  p.y = 5;
-  p.z = 7;
-  cloud->push_back(p);
+
+  auto points = qoaed::tools::read_off(argv[1]);
+  for (auto& t : points) {
+    p.x = t.x;
+    p.y = t.y;
+    p.z = t.z;
+    cloud->push_back(p);
+  }
 
   pcl::visualization::CloudViewer viewer("Viewer");
   viewer.showCloud(cloud);
