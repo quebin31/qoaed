bfd2c88a469946190826efa08dcc838360b0c0ac
Kevin <lans9831@gmail.com>
2017-12-31 03:00:55 -0500

    fixed what_octant for point octree

--- test/PCLTest.cpp
+++ test/PCLTest.cpp
@@ -2,35 +2,40 @@
 #include <pcl/visualization/point_picking_event.h>
 #include <iostream>
 
-#include "Point.hpp"
 #include "Tools.hpp"
+#include "PointOctree.hpp"
 
 
-void foo(const pcl::visualization::PointPickingEvent& e, void* something) {
+void foo(const pcl::visualization::PointPickingEvent& e, void* point) {
   float x, y, z;
   e.getPoint(x,y,z);
-  qoaed::Point3D<float> *p = (qoaed::Point3D<float>*) (something);
-  p->x = x;
-  p->y = y;
-  p->z = z;
+  qoaed::Point3D<double> *p = (qoaed::Point3D<double>*) (point);
+  p->x = (double) x;
+  p->y = (double) y;
+  p->z = (double) z;
   std::cout << p->x << ' ' << p->y << ' ' << p->z << std::endl;
 }
 
 int main(int argc, char** argv) {
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+  //qoaed::PointOctree<pcl::PointXYZRGB, double> octree;
 
-  pcl::PointXYZ p;
+  pcl::PointXYZRGB p;
   auto points = qoaed::tools::read_off(argv[1]);
-  for (auto& t : points) {
+  for (qoaed::Point3D<double>& t : points) {
     p.x = t.x;
     p.y = t.y;
     p.z = t.z;
+    p.r = 255;
+    p.g = 255;
+    p.b = 255;
     cloud->push_back(p);
+    //octree.insert(t, p);
   }
 
-  qoaed::Point3D<float> pqoaed;
+  qoaed::Point3D<double> pqoaed;
   pcl::visualization::CloudViewer viewer("Viewer");
-  viewer.registerPointPickingCallback(foo, &p);
+  viewer.registerPointPickingCallback(foo, &pqoaed);
   viewer.showCloud(cloud);
   viewer.showCloud(cloud);
   while (!viewer.wasStopped()) {}
