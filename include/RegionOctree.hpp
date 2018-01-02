b658a3b7041ae40d98c2ca6d1f208f1f509ec179
Unknown <mb.lev17@gmail.com>
2018-01-02 12:41:10 -0500

    to point

--- include/RegionOctree.hpp
+++ include/RegionOctree.hpp
@@ -4,16 +4,19 @@
 #include <map>
 #include <array>
 
+#include "Point.hpp"
+
+namespace qoaed {
 template <class Value, class Key = long>
 class RegionOctree {
 private:
 
   struct Obj {
-    Key x, y, z;
+    Point3D<Key> point;
     mutable Value val;
 
     Obj(const Key& x, const Key& y,const Key& z, const Value& val) :
-      x(x), y(y), z(z), val(val) {}
+    point(x,y,z), val(val) {}
   };
 
   struct Node;
@@ -21,48 +24,48 @@ private:
   using Box    = typename std::map<Key, std::map<Key, std::map<Key, Obj> > >;
 
   struct Node {
-    Key min_x, min_y, min_z;
-    Key max_x, max_y, max_z;
+    Point3D<Key> min;
+    Point3D<Key> max;
     Childs  childs;
     Box     box;
 
     Node(const Key& minx, const Key& miny, const Key& minz, const Key& maxx, const Key& maxy, const Key& maxz) :
-      min_x(minx), min_y(miny), min_z(minz), max_x(maxx), max_y(maxy), max_z(maxz)
+      min(minx,miny,minz), max(maxx,maxy,maxz)
       { childs = {0, 0, 0, 0, 0, 0, 0, 0}; }
 
-    void insert(const Key& x, const Key& y, const Key& z, const Value& val) {
-      auto xit = box.find(x);
+    void insert(const Point3D<Key>& p, const Value& val) {
+      auto xit = box.find(p.x);
       if (xit == box.end()) {
-        auto insertedx = box.emplace(x, std::map<Key,std::map<Key, Obj>() >());
-        auto yit = insertedx->second.find(y);
+        auto insertedx = box.emplace(p.x, std::map<Key,std::map<Key, Obj>() >());
+        auto yit = insertedx->second.find(p.y);
         if(yit == insertedx->second.end()){
-          auto insertedy = insertedx.emplace(y, std::map<Key,Obj>());
-          std::get<0>(insertedy)->second.emplace(z, Obj(x,y,z,val));
+          auto insertedy = insertedx.emplace(p.y, std::map<Key,Obj>());
+          std::get<0>(insertedy)->second.emplace(p.z, Obj(p.x,p.y,p.z,val));
         } else{
-          auto zit = yit->second.find(z);
+          auto zit = yit->second.find(p.z);
           if(zit==yit->second.end()){
-            zit->second.emplace(z, Obj(x,y,z,val));
+            zit->second.emplace(p.z, Obj(p.x,p.y,p.z,val));
           } else {
             return;
           }
         }
       } else {
-        auto yit = xit->second.find(y);
+        auto yit = xit->second.find(p.y);
         if (yit == xit->second.end())
-          xit->second.emplace(y, Obj(x,y,z,val));
+          xit->second.emplace(p.y, Obj(p.x,p.y,p.z,val));
         else
           return;
       }
     }
 
-    void remove(const Key& x, const Key& y, const Key& z) {
-      auto xit = box.find(x);
+    void remove(const Point3D<Key>& p) {
+      auto xit = box.find(p.x);
       if (xit == box.end()) return;
 
-      auto yit = xit->second.find(y);
+      auto yit = xit->second.find(p.y);
       if (yit == xit->second.end()) return;
 
-      auto zit = yit->second.find(z);
+      auto zit = yit->second.find(p.z);
       if (zit == yit->second.end()) return;
 
       yit->second.erase(zit);
@@ -73,16 +76,16 @@ private:
       }
     }
 
-    const Obj& find(const Key& x, const Key& y, const Key& z) {
-      auto xit = box.find(x);
+    const Obj& find(const Point3D<Key>& p) {
+      auto xit = box.find(p.x);
       if (xit == box.end())
         throw std::runtime_error("Cannot found x coord");
 
-      auto yit = xit->second.find(y);
+      auto yit = xit->second.find(p.y);
       if (yit == xit->second.end())
         throw std::runtime_error("Cannot found y coord");
 
-      auto zit = yit->second.find(z);
+      auto zit = yit->second.find(p.z);
       if(zit == yit->second.end())
         throw std:: runtime_error("Cannot found z coord");
 
@@ -97,55 +100,62 @@ private:
   Node* m_root;
 
 public:
-  RegionOctree(const Key& max_x, const Key& max_y, const Key& max_z) : m_root(new Node(0, 0, 0, max_x, max_y, max_z)) {}
+  RegionOctree(const Point3D<Key>& p) : m_root(new Node(0, 0, 0, p.x, p.y, p.z)) {}
  ~RegionOctree() = default;
 
-  void insert(const Key& x, const Key& y, const Key& z, const Value& val) {
+  void insert(const Point3D<Key>& p, const Value& val) {
     Node** tmp;
-    if (find(x,y,z,tmp)) return;
-    (*tmp) = new Node(x,y,z,val);
+    if (find(p.x,p.y,p.z,tmp)) return;
+    (*tmp) = new Node(p.x,p.y,p.z,val);
+  }
+  void insert(const Key& x, const Key& y, const Key& z, Value& val){
+    insert(Point3D<Key>(x,y,z), val);
   }
 
 private:
 
-  bool find(const Key& x, const Key& y, const Key& z, Node**& node) {
+  bool find(const Point3D<Key>& p, Node**& node) {
     node = &m_root;
     while (*node) {
       try {
-        (*node)->find(x,y,z);
+        (*node)->find(p);
         return true;
       } catch(std::exception& e) {
-        int pos = what_quadrant(x,y,z,*node);
+        int pos = what_quadrant(p,*node);
         node=&((*node)->childs[pos]);
       }
     }
     return false;
   }
 
-  int what_octant(const Key& x, const Key& y, const Key& z, Node* n) {
+  bool find(const Key& x, const Key& y,const Key& z, Node**& node){
+    return find(Point3D<Key>(x,y,z), node);
+  }
+
+  int what_octant(const Point3D<Key>& p, Node* n) {
       Key middle_x = (n->min_x + n->max_x)/Key(2);
       Key middle_y = (n->min_y + n->max_y)/Key(2);
       Key middle_z = (n->min_z + n->max_z)/Key(2);
 
       // TODO: Esta bien que todas las comparaciones sean menor o igual ; mayor o igual?
-      if(z>=n->min_z && z<=middle_z){
-        if(x>=middle_x && x<=n->max_x){
-            if(y>=middle_y && y<=n->max_y) return 0;
-            else if(y>=n->min_y && y<middle_y) return 3;
+      if(p.z>=n->min_z && p.z<=middle_z){
+        if(p.x>=middle_x && p.x<=n->max_x){
+            if(p.y>=middle_y && p.y<=n->max_y) return 0;
+            else if(p.y>=n->min_y && p.y<middle_y) return 3;
         }
-        else if(x>=n->min_x && x<middle_x){
-            if(y>=middle_y && y<=n->max_y) return 1;
-            else if(y>=n->min_y && y<middle_y) return 2;
+        else if(p.x>=n->min_x && p.x<middle_x){
+            if(p.y>=middle_y && p.y<=n->max_y) return 1;
+            else if(p.y>=n->min_y && p.y<middle_y) return 2;
         }
       }
-      else if(z>middle_z && z<=n->max_z){
-        if(x>=middle_x && x<=n->max_x){
-            if(y>=middle_y && y<=n->max_y) return 5;
-            else if(y>=n->min_y && y<middle_y) return 4;
+      else if(p.z>middle_z && p.z<=n->max_z){
+        if(p.x>=middle_x && p.x<=n->max_x){
+            if(p.y>=middle_y && p.y<=n->max_y) return 5;
+            else if(p.y>=n->min_y && p.y<middle_y) return 4;
         }
-        else if(x>=n->min_x && x<middle_x){
-            if(y>=middle_y && y<=n->max_y) return 6;
-            else if(y>=n->min_y && y<middle_y) return 7;
+        else if(p.x>=n->min_x && p.x<middle_x){
+            if(p.y>=middle_y && p.y<=n->max_y) return 6;
+            else if(p.y>=n->min_y && p.y<middle_y) return 7;
         }
       }
   }
@@ -167,6 +177,6 @@ private:
   }
 
 };
-
+}
 
 #endif
